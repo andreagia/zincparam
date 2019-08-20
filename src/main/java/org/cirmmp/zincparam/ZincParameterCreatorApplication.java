20c9a8001b2cbdba39573c6bc259855997e366d3
Andrea <andreagia71@gmail.com>
2019-08-20 16:14:04 +0200

    Initial commit

---
+++ src/main/java/org/cirmmp/zincparam/ZincParameterCreatorApplication.java
@@ -0,0 +1,13 @@
+package org.cirmmp.zincparam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZincParameterCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZincParameterCreatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cirmmp/zincparam/ZincParameterCreatorApplicationTests.java
@@ -0,0 +1,16 @@
+package org.cirmmp.zincparam;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZincParameterCreatorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
